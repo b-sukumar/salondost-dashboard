b23db4cc34d0370f9642f6da449ca84e8e13a420
Sukumar Behera <sukumarbehera@Sukumars-MacBook-Air.local>
2026-01-28 16:37:40 +0530

    chore: nuclear test - hardcode keys and disable authguard

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,16 +1,16 @@
 import { createClient } from '@supabase/supabase-js'
 
+const sbUrl = 'https://trosqchwggudldwtzbno.supabase.co'
+const sbKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyb3NxY2h3Z2d1ZGxkd3R6Ym5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1MzUwODgsImV4cCI6MjA4NTExMTA4OH0.8a5SpZauGvD9byx9mwCJanRKjQF__nCQdocQ0ySd5Qo'
+
 export const supabase = createClient(
-    process.env.NEXT_PUBLIC_SB_URL!,
-    process.env.NEXT_PUBLIC_SB_ANON_KEY!,
+    sbUrl,
+    sbKey,
     {
         auth: {
-            persistSession: true,
+            persistSession: false,
             detectSessionInUrl: true,
             flowType: 'implicit'
-        },
-        global: {
-            headers: { 'x-my-custom-header': 'salondost' }
         }
     }
 )
